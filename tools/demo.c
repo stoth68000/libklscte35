d8fae2a08a72aab58ec097a67c46618db1a86037
Steven Toth <stoth@kernellabs.com>
2016-09-16 11:57:06 -0400

    Initial Checkin

---
+++ tools/demo.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <libklscte35/scte35.h>
+
+static void hexdump(unsigned char *buf, unsigned int len, int bytesPerRow /* Typically 16 */)
+{
+	for (unsigned int i = 0; i < len; i++)
+		printf("%02x%s", buf[i], ((i + 1) % bytesPerRow) ? " " : "\n");
+	printf("\n");
+}
+
+int demo_main(int argc, char *argv[])
+{
+	struct scte35_context_s s35, *scte35;
+	scte35 = &s35;
+
+	scte35_initialize(scte35, 0x0123);
+
+	/* Generate a OUT_OF_NETWORK IMMEDIATE section */
+	printf("Generating out of network table section\n");
+	scte35_set_next_event_id(scte35, 1);
+	scte35_generate_immediate_out_of_network(scte35, 0x1000);
+	hexdump(scte35->section, scte35->section_length, 32);
+
+	/* Generate a return IN_TO_NETWORK IMMEDIATE section */
+	printf("Generating back to network table section\n");
+	scte35_set_next_event_id(scte35, 1);
+	scte35_generate_immediate_out_of_network(scte35, 0x1000);
+	hexdump(scte35->section, scte35->section_length, 32);
+
+	printf("program complete.\n");
+	return 0;
+}
---
+++ tools/klscte35_util.c
@@ -0,0 +1,41 @@
+/* Copyright Kernel Labs Inc 2016 */
+
+#include <stdio.h>
+#include <string.h>
+#include <libgen.h>
+
+/* External tool hooks */
+extern int demo_main(int argc, char *argv[]);
+
+typedef int (*func_ptr)(int, char *argv[]);
+
+int main(int argc, char *argv[])
+{
+	struct app_s {
+		char *name;
+		func_ptr func;
+	} apps[] = {
+		{ "klscte35_util",		demo_main, },
+		{ 0, 0 },
+	};
+	char *appname = basename(argv[0]);
+
+	int i = 0;
+	struct app_s *app = &apps[i++];
+	while (app->name) {
+		if (strcmp(appname, app->name) == 0)
+			return app->func(argc, argv);
+
+		app = &apps[i++];
+	}
+
+	printf("No application called %s, aborting.\n", appname);
+	i = 0;
+	app = &apps[i++];
+	while (app->name) {
+		printf("%s ", app->name);
+		app = &apps[i++];
+	}
+
+	return 1;
+}
