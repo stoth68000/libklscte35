fd94e0d887a8723628233c8711536fba060e3123
Steven Toth <stoth@kernellabs.com>
2016-10-03 17:44:33 -0400

    Sample code cleanup for scte35 parsing.

--- tools/parse.c
+++ tools/parse.c
@@ -3,9 +3,6 @@
 #include <stdint.h>
 #include <libklscte35/scte35.h>
 
-// push this into the klscte35
-#include <bitstream/scte/35.h>
-
 static void hexdump(unsigned char *buf, unsigned int len, int bytesPerRow /* Typically 16 */)
 {
 	for (unsigned int i = 0; i < len; i++)
@@ -15,9 +12,9 @@ static void hexdump(unsigned char *buf, unsigned int len, int bytesPerRow /* Typ
 
 /* Mouse:
    Out of network:
-<SCTE35 command="5" command_str="insert" pts_adjustment="67521" event_id="692" cancel="0" out_of_network="1" program_splice="1" splice_time="immediate" unique_program_id="1">
+   <SCTE35 command="5" command_str="insert" pts_adjustment="67521" event_id="692" cancel="0"
+      out_of_network="1" program_splice="1" splice_time="immediate" unique_program_id="1">
 */
-
 uint8_t mouse_oon[] = {
 	0xfc, 0x30, 0x1b, 0x00, 0x00, 0x00, 0x01, 0x07,
 	0xc1, 0x00, 0xff, 0xf0, 0x0a, 0x05, 0x00, 0x00,
@@ -28,13 +25,20 @@ uint8_t mouse_oon[] = {
 /*
    Mouse:
    Back to network:
-   fc 30 20 00 00 00 01 07 c1 00 ff f0 0f 05 00 00 02 b5 7f 4f fe 85 99 84 e4 00 01 01 01 00 00 3e 17 d8 2e 
-<SCTE35 command="5" command_str="insert" pts_adjustment="67521" event_id="693" cancel="0" out_of_network="0" program_splice="1" splice_time="2241430756" unique_program_id="1">
+   <SCTE35 command="5" command_str="insert" pts_adjustment="67521" event_id="693" cancel="0"
+      out_of_network="0" program_splice="1" splice_time="2241430756" unique_program_id="1">
 */
+uint8_t mouse_btn[] = {
+	0xfc, 0x30, 0x20, 0x00, 0x00, 0x00, 0x01, 0x07,
+	0xc1, 0x00, 0xff, 0xf0, 0x0f, 0x05, 0x00, 0x00,
+	0x02, 0xb5, 0x7f, 0x4f, 0xfe, 0x85, 0x99, 0x84,
+	0xe4, 0x00, 0x01, 0x01, 0x01, 0x00, 0x00, 0x3e,
+	0x17, 0xd8, 0x2e 
+};
 
-
-static int test01(uint8_t *sec, int byteCount)
+static int parse(uint8_t *sec, int byteCount)
 {
+	printf("\nParsing a new section......\n");
 	struct scte35_splice_info_section_s *s = scte35_splice_info_section_parse(sec, byteCount);
 	if (s) {
 		scet35_splice_info_section_print(s);
@@ -46,7 +50,8 @@ static int test01(uint8_t *sec, int byteCount)
 
 int parse_main(int argc, char *argv[])
 {
-	test01(&mouse_oon[0], sizeof(mouse_oon));
+	parse(&mouse_oon[0], sizeof(mouse_oon));
+	parse(&mouse_btn[0], sizeof(mouse_btn));
 
 	printf("program complete.\n");
 	return 0;
