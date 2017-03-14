e5aba2a5ef6d6ef54e7726002e43c65dc3ffd4f9
Devin Heitmueller <dheitmueller@kernellabs.com>
2017-03-14 15:50:55 -0400

    Fix memory leak referenced in Coverity CID 1418136
    Also, check the result of scte35_splice_info_section_alloc() for
    a NULL return value.

--- src/scte35.c
+++ src/scte35.c
@@ -387,6 +387,8 @@ int scte35_generate_out_of_network_duration(uint16_t uniqueProgramId, uint32_t e
 					    uint16_t availsExpected)
 {
 	struct scte35_splice_info_section_s *si = scte35_splice_info_section_alloc(SCTE35_COMMAND_TYPE__SPLICE_INSERT);
+	if (si == NULL)
+		return -1;
 	si->splice_insert.splice_event_id = eventId;
 	si->splice_insert.splice_event_cancel_indicator = 0;
 	si->splice_insert.out_of_network_indicator = 1;
@@ -401,8 +403,10 @@ int scte35_generate_out_of_network_duration(uint16_t uniqueProgramId, uint32_t e
 
 	int l = 4096;
 	uint8_t *buf = calloc(1, l);
-	if (!buf)
+	if (!buf) {
+		scte35_splice_info_section_free(si);
 		return -1;
+	}
 
 	ssize_t packedLength = scte35_splice_info_section_packTo(si, buf, l);
 	if (packedLength < 0) {
