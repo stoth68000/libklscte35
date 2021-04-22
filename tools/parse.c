28f2c5367e3afb4d3b7a6505e3818e40a29949f4
Devin Heitmueller <dheitmueller@ltnglobal.com>
2021-04-22 10:15:59 -0400

    Extend test tool to compare original blobs to output
    In order to exercise the parser and generator, after we've parsed
    the packet feed it back into the generator and compare the resulting
    binary blob against the original.
    
    Note that with this commit, the results will show a number of
    failures (i.e. not all tests pass currently).  The pass rate will
    increase with subsequent patches to the parser/generators in the
    library itself.

--- tools/parse.c
+++ tools/parse.c
@@ -24,14 +24,16 @@
 #include <stdint.h>
 #include <libklscte35/scte35.h>
 
-#if 0
+/* Counters to keep track of test results */
+int success=0;
+int fail=0;
+
 static void hexdump(unsigned char *buf, unsigned int len, int bytesPerRow /* Typically 16 */)
 {
 	for (unsigned int i = 0; i < len; i++)
 		printf("%02x%s", buf[i], ((i + 1) % bytesPerRow) ? " " : "\n");
 	printf("\n");
 }
-#endif
 
 /* Mouse:
    Out of network:
@@ -237,15 +239,53 @@ static uint8_t scte35_2019_14_8 [] = {
 static int parse(uint8_t *sec, int byteCount)
 {
 	struct scte35_splice_info_section_s *s = scte35_splice_info_section_parse(sec, byteCount);
+	unsigned char buf[1024];
+	int ret;
+
 	if (s) {
 
 		/* Dump struct to console */
 		scte35_splice_info_section_print(s);
 
+                /* Verify our generator creates the same payload */
+		memset(buf, 0, sizeof(buf));
+		ret = scte35_splice_info_section_packTo(s, buf, sizeof(buf));
+		if (ret < 0) {
+			printf("Failed to create section\n");
+			fail++;
+			return 0;
+		}
+
+		if (ret == byteCount && memcmp(sec, buf, byteCount) == 0) {
+			printf("Identical!\n");
+			success++;
+		} else if (ret == byteCount && memcmp(sec+2, buf+2, byteCount-6) == 0) {
+			printf("Only difference is Reserved padding at start of segment!\n");
+			success++;
+		} else {
+			struct scte35_splice_info_section_s *generated;
+			printf("Different!\n");
+			fail++;
+
+			/* Show the differences */
+			printf("Orig (len=%d)\n", byteCount);
+			hexdump(sec, byteCount, 16);
+			printf("New (len=%d)\n", ret);
+			if (ret >= 0)
+				hexdump(buf, ret, 16);
+
+			generated = scte35_splice_info_section_parse(buf, ret);
+			if (generated) {
+				scte35_splice_info_section_print(generated);
+				scte35_splice_info_section_free(generated);
+			}
+		}
 		/* Free the allocated resource */
 		scte35_splice_info_section_free(s);
 
 		printf("\n");
+	} else {
+		fail++;
 	}
 
 	return 0;
@@ -291,5 +331,6 @@ int parse_main(int argc, char *argv[])
 	parse(&scte35_2019_14_8[0], sizeof(scte35_2019_14_8));
 
 	printf("program complete.\n");
+	printf("Success=%d Failures=%d\n", success, fail);
 	return 0;
 }
